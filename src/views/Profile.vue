29b23507d0da137e716aff56dd000ab011fda5ee
noodles <3378818993@qq.com>
2026-04-28 21:00:23 +0800

    新增：企业认证面板组件及个人主页入口

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,5 +2,13 @@
   <div class="profile-container">
     <h1>个人主页</h1>
     <p>欢迎回来，这是你的投票系统管理后台。</p>
-    </div>
+    
+    <EnterprisePanel />
+    
+  </div>
 </template>
+
+<script setup>
+// 告诉当前页面，去哪里找到我们的组件代码
+import EnterprisePanel from '../components/EnterprisePanel.vue'
+</script>
